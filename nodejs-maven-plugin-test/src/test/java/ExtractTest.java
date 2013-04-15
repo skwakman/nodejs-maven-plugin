d0413103a8867349e255b34b9754e2fa9be5c512
Sannie Kwakman <skwakman@xebia.com>
2013-04-15 22:50:32 +0200

    Unit test is now compatible for non-windows platforms

--- nodejs-maven-plugin-test/src/test/java/ExtractTest.java
+++ nodejs-maven-plugin-test/src/test/java/ExtractTest.java
@@ -1,7 +1,10 @@
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Collection;
 
 public class ExtractTest {
 
@@ -11,7 +14,21 @@ public class ExtractTest {
 		File extractedDir = new File(System.getProperty("testOutputDir") + "/nodejs/extracted/");
 		Assert.assertTrue("extractedDir " + extractedDir.getAbsolutePath() + " does not exist", extractedDir.exists());
 
-		File extractedNodeJsExec = new File(extractedDir, "node.exe");
+    Collection<File> foundFiles = FileUtils.listFiles(extractedDir, new IOFileFilter() {
+      @Override
+      public boolean accept(File file) {
+        return file.getName().startsWith("node");
+      }
+
+      @Override
+      public boolean accept(File dir, String name) {
+        return name.startsWith("node");
+      }
+    }, null);
+
+    Assert.assertEquals(1, foundFiles.size());
+
+    File extractedNodeJsExec = foundFiles.iterator().next();
 		Assert.assertTrue("extracted node JS executable does not exist", extractedNodeJsExec.exists());
 		Assert.assertTrue("extracted node JS executable is empty", extractedNodeJsExec.length() > 1000);
 		Assert.assertTrue("extracted node JS executable did not have executable rights", extractedNodeJsExec.canExecute());
